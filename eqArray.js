c14b8f9de1f050895adf82689bddecff7f9b9000
Kevin Lee <kevin.skyulee@gmail.com>
2020-08-18 21:27:46 +0000

    added eqArray to check array equality

---
+++ eqArray.js
@@ -0,0 +1,26 @@
+const assertEqual = (actual, expected) => {
+
+  typeof(actual) === "string" ? actual = `"${actual}"` : null;
+  typeof(expected) === "string" ? expected = `"${expected}"` : null;
+
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} === ${expected}`);
+  }
+};
+
+const eqArrays = (actual, expected) => {
+  return actual.length === expected.length && actual.every((val, index) => val === expected[index])
+   ? true : false
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
