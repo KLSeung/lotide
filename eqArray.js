48dee5610bd1c0be54682e3ea8e04e4de52358e2
Kevin Lee <kevin.skyulee@gmail.com>
2020-08-22 07:54:29 +0000

    changed eqArrays function to be recursive

--- eqArray.js
+++ eqArray.js
@@ -10,10 +10,34 @@ const assertEqual = (actual, expected) => {
   }
 };
 
+// const eqArrays = (actual, expected) => {
+//   return actual.length === expected.length && actual.every((val, index) => val === expected[index])
+//     ? true : false;
+// };
+
 const eqArrays = (actual, expected) => {
-  return actual.length === expected.length && actual.every((val, index) => val === expected[index])
-    ? true : false;
+  if (actual.length !== expected.length) {
+    return false;
+  } else {
+    for (let i = 0; i < actual.length; i++) {
+      if (Array.isArray(actual[i]) || Array.isArray(expected[i])) {
+        if (!eqArrays(actual[i], expected[i])) {
+          return false;
+        }
+      } else {
+        if (actual[i] !== expected[i]) {
+          return false;
+        }
+      }
+    }
+  }
+  return true;
 };
+
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])); // => true
+
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])); // => false
+console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])); // => false
 
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
