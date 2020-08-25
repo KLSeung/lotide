e145c9ef65bf5489ec1f6c32efb524d9f8251b9f
Kevin Lee <kevin.skyulee@gmail.com>
2020-08-25 00:30:05 +0000

    added test for middle

--- middle.js
+++ middle.js
@@ -1,20 +1,3 @@
-const eqArrays = (actual, expected) => {
-  return actual.length === expected.length && actual.every((val, index) => val === expected[index])
-    ? true : false;
-};
-
-const assertArraysEqual = (actual, expected) => {
-  for (let i = 0; i < actual.length; i++) {
-    typeof(actual[i]) === "string" ? actual[i] = `"${actual[i]}"` : null;
-    typeof(expected[i]) === "string" ? expected[i] = `"${expected[i]}"` : null;
-  }
-  if (eqArrays(actual, expected)) {
-    console.log(`🟢🟢🟢 Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-
 const middle = (arr) => {
   let newArray = [];
   let midIndex = [];
@@ -34,11 +17,4 @@ const middle = (arr) => {
   return newArray;
 };
 
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual(middle([1, 2]), []); // => []
-
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
+module.exports = middle;
