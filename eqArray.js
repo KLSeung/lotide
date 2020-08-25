f3e728ba2529b1c249c59037068f4a3dcd8fb006
Kevin Lee <kevin.skyulee@gmail.com>
2020-08-25 00:16:41 +0000

    added test for tail function and also added mocha and chai dependencies

--- eqArray.js
+++ eqArray.js
@@ -1,20 +1,3 @@
-const assertEqual = (actual, expected) => {
-
-  typeof(actual) === "string" ? actual = `"${actual}"` : null;
-  typeof(expected) === "string" ? expected = `"${expected}"` : null;
-
-  if (actual === expected) {
-    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} === ${expected}`);
-  }
-};
-
-// const eqArrays = (actual, expected) => {
-//   return actual.length === expected.length && actual.every((val, index) => val === expected[index])
-//     ? true : false;
-// };
-
 const eqArrays = (actual, expected) => {
   if (actual.length !== expected.length) {
     return false;
@@ -34,17 +17,4 @@ const eqArrays = (actual, expected) => {
   return true;
 };
 
-console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])); // => true
-
-console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])); // => false
-console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])); // => false
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+module.exports = eqArrays;
